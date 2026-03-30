18cd748ad0312efb5d7b82e2ca0cb6755b8b7f60
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-30 09:29:39 +0100

    ajoute le model medecin

---
+++ app/Models/Medecin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Medecin extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'specialite',
+        'is_validated'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function rendezVous()
+    {
+        return $this->hasMany(RendezVous::class);
+    }
+}
