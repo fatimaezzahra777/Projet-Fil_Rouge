d2d140732c0ca4ece7272787adbecb53cc9ba8b3
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-30 09:28:54 +0100

    ajoute le model association

--- app/Models/Association.php
+++ app/Models/Association.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Association extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'specialite',
+        'is_validated'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function rendezVous()
+    {
+        return $this->hasMany(RendezVous::class);
+    }
 }
