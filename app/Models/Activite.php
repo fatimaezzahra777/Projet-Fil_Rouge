209103e9e7d132c4a59f3dd6ff6f220867470156
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-30 09:27:51 +0100

    ajoute le model activite

--- app/Models/Activite.php
+++ app/Models/Activite.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activite extends Model
 {
-    //
+    protected $fillable = [
+        'association_id',
+        'titre',
+        'description',
+        'date',
+        'points'
+    ];
+
+    public function association()
+    {
+        return $this->belongsTo(Association::class);
+    }
+
+    public function participations()
+    {
+        return $this->hasMany(Participation::class);
+    }
 }
