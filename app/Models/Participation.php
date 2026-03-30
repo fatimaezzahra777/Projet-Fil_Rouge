9326c0b5e940bacd76dc4941ba3c17592ab73ba6
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-30 09:30:08 +0100

    ajoute le model participation

--- app/Models/Participation.php
+++ app/Models/Participation.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participation extends Model
 {
-    //
+    protected $fillable = [
+        'patient_id',
+        'activite_id',
+        'statut'
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function activite()
+    {
+        return $this->belongsTo(Activite::class);
+    }
 }
