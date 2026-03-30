42ee0c18f205af69d699805d2df251d62ff7c399
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-30 09:31:08 +0100

    ajoute le model rendez vous

--- app/Models/RendezVous.php
+++ app/Models/RendezVous.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class RendezVous extends Model
 {
-    //
+    protected $table = 'rendez_vous';
+
+    protected $fillable = [
+        'patient_id',
+        'medecin_id',
+        'date',
+        'heure',
+        'statut'
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function medecin()
+    {
+        return $this->belongsTo(Medecin::class);
+    }
 }
