4fa71954eee492c3ed23d13f6301f5a966e6b43e
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-30 09:31:39 +0100

    ajoute le model transaction des points

--- app/Models/TransactionPoint.php
+++ app/Models/TransactionPoint.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransactionPoint extends Model
 {
-    //
+    protected $table = 'transactions_points';
+
+    protected $fillable = [
+        'patient_id',
+        'montant',
+        'type',
+        'description'
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
