76267336178986e88243cfd553cab2ff890bd226
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-30 09:30:35 +0100

    ajoute le model patient

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'type_addiction',
+        'points'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function rendezVous()
+    {
+        return $this->hasMany(RendezVous::class);
+    }
+
+    public function participations()
+    {
+        return $this->hasMany(Participation::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(TransactionPoint::class);
+    }
 }
