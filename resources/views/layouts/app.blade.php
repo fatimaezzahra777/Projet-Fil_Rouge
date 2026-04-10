ad8c1c29029799a3849940e4e2074b1c52cf1126
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-04-10 12:03:52 +0100

    ajoute des modification dans app view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,11 +14,11 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+   <body class="font-sans antialiased bg-gray-100">
+
+        <div class="min-h-screen">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
             @isset($header)
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -27,10 +27,14 @@
                 </header>
             @endisset
 
-            <!-- Page Content -->
             <main>
-                {{ $slot }}
+                {{ $slot ?? '' }}
+
+                @hasSection('content')
+                    @yield('content')
+                @endif
             </main>
         </div>
+
     </body>
 </html>
