c279d2093f6a73b0dd63fec92fe42935dd53a049
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-04-26 15:47:37 +0100

    ajoute des modification dans Medecin controller

--- app/Http/Controllers/MedecinController.php
+++ app/Http/Controllers/MedecinController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Medecin;
+use Illuminate\Support\Facades\Schema;
 
 class MedecinController extends Controller
 {
@@ -12,6 +13,8 @@ class MedecinController extends Controller
      */
     public function index()
     {
+        abort_unless(auth()->user()?->hasRole('admin'), 403);
+
         $medecins = Medecin::with('user')->get();
         return view('medecins.index', compact('medecins'));
     }
@@ -37,6 +40,8 @@ class MedecinController extends Controller
      */
     public function show(string $id)
     {
+        abort_unless(auth()->user()?->hasRole('admin'), 403);
+
         $medecin = Medecin::with('user')->findOrFail($id);
         return view('medecins.show', compact('medecin'));
     }
@@ -54,10 +59,35 @@ class MedecinController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        abort_unless(auth()->user()?->hasRole('admin'), 403);
+
         $medecin = Medecin::findOrFail($id);
         $medecin->update(['is_validated' => true]);
 
         return back()->with('success', 'Médecin validé');
+    }
+
+    public function updateAppointmentCost(Request $request)
+    {
+        $user = auth()->user();
+
+        abort_unless($user?->hasRole('medecin') && $user->medecin, 403);
+
+        $validated = $request->validate([
+            'appointment_points_cost' => ['required', 'integer', 'min:1'],
+        ]);
+
+        if (! Schema::hasColumn('medecins', 'appointment_points_cost')) {
+            return back()->withErrors([
+                'appointment_points_cost' => 'La colonne du cout par rendez-vous n existe pas encore. Lancez php artisan migrate.',
+            ]);
+        }
+
+        $user->medecin->update([
+            'appointment_points_cost' => $validated['appointment_points_cost'],
+        ]);
+
+        return back()->with('success', 'Points par rendez-vous mis a jour.');
     }
 
     /**
