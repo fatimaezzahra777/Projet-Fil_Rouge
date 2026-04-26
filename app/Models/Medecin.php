893403fe3bf9983f9373e2790b425ebafbd638b6
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-04-26 16:21:48 +0100

    ajoute des modification dans model de les medecins

--- app/Models/Medecin.php
+++ app/Models/Medecin.php
@@ -6,9 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medecin extends Model
 {
+    public const DEFAULT_APPOINTMENT_POINTS_COST = 20;
+
     protected $fillable = [
         'user_id',
         'specialite',
+        'appointment_points_cost',
+        'points',
         'is_validated'
     ];
 
