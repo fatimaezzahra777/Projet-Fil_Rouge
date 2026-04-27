57237f8b450d2c4032eab6ec2e838ce386463c18
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-04-27 09:58:39 +0100

    ajoute une nouvelle migrations pour les points

--- database/migrations/2026_04_20_100000_add_appointment_points_cost_to_medecins_table.php
+++ database/migrations/2026_04_20_100000_add_appointment_points_cost_to_medecins_table.php
@@ -3,7 +3,6 @@
 use App\Models\Medecin;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,12 +17,6 @@ return new class extends Migration
                 ->default(Medecin::DEFAULT_APPOINTMENT_POINTS_COST)
                 ->after('specialite');
         });
-
-        if (Schema::hasColumn('medecins', 'points')) {
-            DB::table('medecins')
-                ->where('points', '>', 0)
-                ->update(['appointment_points_cost' => DB::raw('points')]);
-        }
     }
 
     /**
